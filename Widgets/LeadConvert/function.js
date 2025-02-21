3a7da793594d9533a1ce084f40a6a68fb136996c
Ali Raza <eng.alisiddiqui@gmail.com>
2025-02-21 21:50:03 +0500

    Add files via upload

---
+++ Widgets/LeadConvert/function.js
@@ -0,0 +1,92 @@
+async function create_account(){
+  account_created =await ZOHO.CRM.API.insertRecord({Entity:"Accounts",APIData:account_map});
+  account_id = account_created.data[0].details.id;
+  if(account_id){
+    console.log('account created',account_id);
+    return account_id;
+  }
+}
+async function create_contact(acct_id){
+  if(acct_id){
+    contact_map.Account_Name = acct_id;
+  }
+  contact_created =await ZOHO.CRM.API.insertRecord({Entity:"Contacts",APIData:contact_map});
+  contact_id = contact_created.data[0].details.id;
+  if(contact_id){
+    console.log('contact created',contact_id);
+    let lead_data = await ZOHO.CRM.API.getRecord({Entity: "Contacts", approved: "both", RecordID:contact_id});
+    lead_rec = lead_data.data[0];
+    cid = lead_rec.Lead_Number1;
+    return { contact_id, cid }; 
+  }
+}
+function lead_update(){
+  var config={
+    Entity:"Leads",
+    APIData:{
+          "id": lead_id[0],
+          "Lead_Status": 'Closed Won'
+    }
+  }
+lead_updated = ZOHO.CRM.API.updateRecord(config);
+}
+async function create_deal(cont_id,acct_id,cID){
+  if(acct_id){
+    deal_map.Account_Name = acct_id;
+  }
+  amount = document.getElementById('amount').value;
+  date = document.getElementById('date').value;
+  stage = document.getElementById('stage').value;
+  apnumber = document.getElementById('apnumber').value;
+  buildingNumber = document.getElementById('buildingNumber').value;
+  // leaderName = document.getElementById('leaderName').value;
+  projectName = document.getElementById('projectName').value;
+  developerName = document.getElementById('developerName').value;
+  source = document.getElementById('source').value;
+  category = document.getElementById('category').value;
+  subcategory = document.getElementById('subcategory').value;
+  initialDeposit = document.getElementById('initialDeposit').value;
+  comment = document.getElementById('comment').value;
+  deal_map.Deal_Name = `${UniqueLead}${cID}${UnitNumber}`;
+  deal_map.Stage = stage;
+  deal_map.Closing_Date = date;
+  deal_map.Amount = amount;
+  deal_map.Contact_Name = cont_id;
+  deal_map.Apartment_Number = apnumber;
+  deal_map.Building_Number = buildingNumber;
+  // deal_map.Team_Leader_Name = leaderName;
+  deal_map.Project_Name = projectName;
+  deal_map.Developer_Name = developerName;
+  deal_map.Lead_Source = source;
+  deal_map.Category = category;
+  deal_map.Sub_Category = subcategory;
+  deal_map.Initial_Deposit = initialDeposit;
+  deal_map.Remarks_Comments = comment;
+  deal_created =await ZOHO.CRM.API.insertRecord({Entity:"Deals",APIData:deal_map});
+  deal_id = deal_created.data[0].details.id;
+  if(deal_id){
+    lead_update();
+    console.log('Deal is created',deal_id);
+    ZOHO.CRM.UI.Record.open({Entity:"Deals",RecordID:deal_id});
+  }
+}
+async function checkvalidation(){
+  if(existing_contact_id){
+    deal_map.Get_Attachment = true;
+    contactid = existing_contact_id;
+    account_created = existing_account_id;
+    cID = UniqueContact;
+  }
+  else{
+    if(agency_name){
+      account_created =await create_account();
+    }
+    else{
+      account_created = '';
+    }
+    result = await create_contact(account_created);
+    contactid = result.contact_id;
+    cID = ` - ${result.cid}`;
+  }
+    create_deal(contactid,account_created,cID);
+}
---
+++ Widgets/LeadConvert/onload.js
@@ -0,0 +1,138 @@
+let lead_id; 
+let existing_contact_id;
+let existing_account_id;
+let agency_name;
+let account_map = {};
+let contact_map = {};
+let deal_map = {};
+let UniqueLead;
+let UniqueContact = '';
+let UnitNumber;
+ZOHO.embeddedApp.on("PageLoad",async function(data){
+    ZOHO.CRM.UI.Resize({height:"700",width:"1100"});
+    lead_id = data.EntityId;
+    let lead_data = await ZOHO.CRM.API.getRecord({Entity: "Leads", approved: "both", RecordID:lead_id});
+    lead_rec = lead_data.data[0];
+    lead_email = lead_rec.Email;
+    lead_phone = lead_rec.Phone;
+    lead_mobile = lead_rec.Mobile;
+    if(lead_email == null || lead_mobile == null || lead_phone == null){
+        alert(`Mobile, Phone and Email field shouldn't empty`);
+        ZOHO.CRM.UI.Popup.closeReload();
+    }
+    if(lead_email != null){
+        search_rec =await ZOHO.CRM.API.searchRecord({Entity:"Contacts",Type:"email",Query:lead_email});
+        if(search_rec.status != "204"){
+            existing_contact_id = search_rec.data[0].id;
+            existing_account = search_rec.data[0].Account_Name;
+            UniqueContact = search_rec.data[0].Lead_Number1;
+            if(UniqueContact){
+                UniqueContact = ` - ${UniqueContact}`;
+            }
+            if(existing_account){
+                existing_account_id = existing_account.id;
+            }
+            console.log('Existing Contact id is ',existing_contact_id);
+            console.log('Existing Account id is ',existing_account_id);
+        }
+    }
+    UniqueLead = lead_rec.Lead_Number;
+    UnitNumber = lead_rec.Unit_Number;
+    if(!UnitNumber){
+        UnitNumber = '';
+    }
+    else{
+        UnitNumber = ` - ${UnitNumber}`
+    }
+    document.getElementById('dealName').value=`${UniqueLead}${UniqueContact}${UnitNumber}`;
+    document.getElementById('buildingNumber').value=lead_rec.Building_Number;
+    document.getElementById('source').value=lead_rec.Lead_Source;
+    telesales_executive = lead_rec.Telesales_Executive;
+    if(telesales_executive){
+        telesales_executive=telesales_executive.id;
+    }
+    agency_name = lead_rec.Agency_Name;
+    if(lead_rec){
+        account_map ={
+            "Account_Name":agency_name,
+            "Phone":lead_rec.Phone,
+            "Website":lead_rec.Referrer_URL,
+            "Meeting_Attended_Status":lead_rec.Meeting_Attended_Status,
+            "gupshupwhatsappmessaging__Gupshup_WhatsApp_Message_Opt_In":lead_rec.gupshupwhatsappmessaging__Gupshup_WhatsApp_Message_Opt_In,
+            "Lead_Type":lead_rec.Lead_Type,
+            "Telesale_Executive":lead_rec.Telesale_Executive,
+            "Billing_City":lead_rec.City,
+            "Billing_State":lead_rec.State,
+            "Billing_Country":lead_rec.Country,
+            "Description":lead_rec.Description,
+            "Href":lead_rec.Href
+        }
+        contact_map = {
+            "Lead_IQ_Number":lead_rec.Lead_Number,
+            "Lead_Source":lead_rec.Lead_Source,
+            "First_Name":lead_rec.First_Name,
+            "Last_Name":lead_rec.Last_Name,
+            "Lead_Status":"Closed Won",
+            "Email":lead_rec.Email,
+            "Phone":lead_rec.Phone,
+            "Typology":lead_rec.Typology,
+            "Mobile":lead_rec.Mobile,
+            "Skype_ID":lead_rec.Skype_ID,
+            "eventbrite__Eventbrite_Id":lead_rec.eventbrite__Eventbrite_Id,
+            "eventbrite__Order_Date":lead_rec.eventbrite__Order_Date,
+            "Telesale_Executive":lead_rec.Telesale_Executive,
+            "Email_Opt_Out":lead_rec.Email_Opt_Out,
+            "Telesales_Executive":telesales_executive,
+            "gupshupwhatsappmessaging__Gupshup_WhatsApp_Message_Opt_In":lead_rec.gupshupwhatsappmessaging__Gupshup_WhatsApp_Message_Opt_In,
+            "Preferred_Language1":lead_rec.Preferred_Language1,
+            "Meeting_Attended_Status":lead_rec.Meeting_Attended_Status,
+            "Secondary_Email":lead_rec.Secondary_Email,
+            "Twitter":lead_rec.Twitter,
+            "eventbrite__Status":lead_rec.eventbrite__Status,
+            "eventbrite__Company":lead_rec.eventbrite__Eventbrite_Company,
+            "Lead_Type":lead_rec.Lead_Type,
+            "Mailing_City":lead_rec.City,
+            "Mailing_State":lead_rec.State,
+            "Mailing_Country":lead_rec.Country,
+            "Description":lead_rec.Description,
+            "Href":lead_rec.Href,
+            "Developer_Preferences":lead_rec.Developer_Preferences,
+            "ad_id":lead_rec.ad_id,
+            "Property_Name":lead_rec.Select_Property_Preference1,
+            "Bedroom_Preference":lead_rec.Select_Number_of_Bedrooms,
+            "utm_ad_group":lead_rec.utm_ad_group,
+            "Property_Preference":lead_rec.Property_Preference,
+            "utm_campaign":lead_rec.utm_campaign1,
+            "fbp":lead_rec.fbp,
+            "GCLID":lead_rec.GCLID,
+            "utm_content":lead_rec.utm_content1,
+            "utm_device":lead_rec.utm_device1,
+            "Referrer_URL":lead_rec.Referrer_URL,
+            "utm_medium":lead_rec.utm_medium1,
+            "Keyword":lead_rec.Single_Line_2,
+            "User_Agent":lead_rec.userAgent,
+            "utm_name":lead_rec.utm_name1,
+            "Campaign_id":lead_rec.campaign_id,
+            "utm_source":lead_rec.utm_source1,
+            "Community_Preference":lead_rec.Community_Preference,
+            "fbc":lead_rec.fbc,
+            "ga":lead_rec.ga,
+            "How_Soon_Are_You_Looking_To_Buy":lead_rec.How_Soon_Are_You_Looking_To_Buy,
+            "IP_Address":lead_rec.utm_term,
+            "Preferable_Budget":lead_rec.Preferable_Budget1,
+            "Owner_Assignment_Date":lead_rec.Owner_Assignment_Date,
+            "Reassigned_Count":lead_rec.Assignment_Number,
+            "Remarks":lead_rec.Remarks1,
+            "Lead":lead_id[0]
+        }
+        deal_map={
+            "Meeting_Attended_Status":lead_rec.Meeting_Attended_Status,
+            "gupshupwhatsappmessaging__Gupshup_WhatsApp_Message_Opt_In":lead_rec.gupshupwhatsappmessaging__Gupshup_WhatsApp_Message_Opt_In,
+            "Lead_Type":lead_rec.Lead_Type,
+            "Telesale_Executive":lead_rec.Telesale_Executive,
+            "Description":lead_rec.Description,
+            "Href":lead_rec.Href
+        }
+    }
+});
+ZOHO.embeddedApp.init();
